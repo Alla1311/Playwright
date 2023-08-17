da4002af6a9e8c5598cc987bb54b0e3e00d03d99
Alla <lialia13111989@mail.ru>
2023-08-17 23:52:37 +0300

    first commit

---
+++ NetologyTest.js
@@ -0,0 +1,14 @@
+const { chromium } = require("playwright");
+
+(async () => {
+  const browser = await chromium.launch({});
+  const page = await browser.newPage();
+  await page.goto("https://netology.ru/");
+  await page.getByRole("link", { name: "Войти" }).click();
+  await page.getByPlaceholder("Email").click();
+  await page.getByPlaceholder("Email").fill("liialia@mail.ru");
+  await page.getByPlaceholder("Пароль").click();
+  await page.getByPlaceholder("Пароль").fill("Rjpenj1988");
+  await page.getByTestId("login-submit-btn").click();
+  await browser.close();
+})();
---
+++ user.js
@@ -0,0 +1,5 @@
+const email = "lialia13111989@mail.ru";
+const password = "Rjpenj1989";
+const invalidEmail = "liialia@mail.ru";
+const invalidPasspor = "Rjpenj1988";
+Module.exports = { email, password, invalidEmail, invalidPasspord };
